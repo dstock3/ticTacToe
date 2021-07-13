55a537e7d2f28f45c7bb942f84280d026aa61387
David Stockdale <dstock3@gmail.com>
2021-07-13 16:08:38 -0500

    Added spaces attribute containing an array of variable representing each possible space.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const gameBoard = () => {
-
+    spaces: [topLeft, topMiddle, topRight, midLeft, center, midRight, bottomLeft, bottomMiddle, bottomRight];
+    const startGame = (spaces) => {
+        for (i = 0; i < spaces.length; i++) {
+            spaces[i] = null;
+        }
+    }
 };
 
 const player = () => {
@@ -8,3 +13,8 @@ const player = () => {
 
 const playerOne = player(); 
 const playerTwo = player();
+
+function renderDisplay(gameBoard){
+    
+
+};
